47b98b70617b90c662fe130def9dfff3326a61a5
Ben Stenhaug <benastenhaug@gmail.com>
2021-01-13 15:50:06 -0800

    get all the paper stuff back on github

---
+++ paper/paper_2_sim.R
@@ -0,0 +1,80 @@
+out_3_biased_items <-
+    tibble(
+        pars = rerun(1, create_pars_compensatory_3_biased_items())
+    ) %>%
+    # sim and fit intuitive
+    mutate(
+        sim = pars %>% map(~ sim_students_compensatory(pars = ., n_ref = 10000, n_foc = 10000, mean_foc = -1)),
+        intuitive_mod = sim %>% map(~ fit_mod_intuitive(.$data, .$groups))
+    ) %>%
+    # dif: AOAA
+    mutate(
+        AOAA_status = sim %>% map(~ dif_AOAA(.$data, .$groups)),
+        AOAA_mod =
+            map2(
+                sim,
+                AOAA_status,
+                ~ mod_flexible(
+                    .x$data,
+                    .x$groups,
+                    which(.y$status == "flex"),
+                    se = TRUE
+                )
+            ),
+        AOAA_final_dif = AOAA_mod %>% map(mod_to_final_dif)
+    ) %>%
+    # dif: AOAA AS
+    mutate(
+        AOAA_AS_status = map2(sim, AOAA_status, ~ dif_AOAA_AS(.x$data, .x$groups, .y)),
+        AOAA_AS_mod =
+            map2(
+                sim,
+                AOAA_AS_status,
+                ~ mod_flexible(
+                    .x$data,
+                    .x$groups,
+                    which(.y$status == "flex"),
+                    se = TRUE
+                )
+            ),
+        AOAA_AS_final_dif = AOAA_AS_mod %>% map(mod_to_final_dif)
+    ) %>%
+    # dif: AOAA OAT
+    mutate(
+        AOAA_OAT_status = map(sim, ~ dif_AOAA_OAT(.$data, .$groups)),
+        AOAA_OAT_mod =
+            map2(
+                sim,
+                AOAA_OAT_status,
+                ~ mod_flexible(
+                    .x$data,
+                    .x$groups,
+                    which(.y$status == "flex"),
+                    se = TRUE
+                )
+            ),
+        AOAA_OAT_final_dif = AOAA_OAT_mod %>% map(mod_to_final_dif)
+    ) %>%
+    # dif: tibs cluster
+    mutate(
+        cluster =
+            intuitive_mod %>%
+            map(mod_to_final_dif) %>%
+            map(~ mutate(., cluster = irt_cluster(difference_in_easy)))
+    ) %>%
+    mutate(cluster_stats = cluster %>% map(get_cluster_stats)) %>%
+    mutate(cluster_status = map2(cluster, cluster_stats, left_join, by = "cluster")) %>%
+    mutate(
+        cluster_mod =
+            map2(
+                sim,
+                cluster_status,
+                ~ mod_flexible(
+                    .x$data,
+                    .x$groups,
+                    which(.y$status == "flex"),
+                    se = TRUE
+                )
+            ),
+        cluster_final_dif = cluster_mod %>% map(mod_to_final_dif)
+    )
---
+++ paper/paper_3_sim.R
@@ -0,0 +1,22 @@
+# NEED TO ADD MORE DIF
+# DO AN EXTREME CASE THEN WORK BACK FROM THERE
+
+# (higher values of b_target are associated with less dif)
+
+# out_noncomp$intuitive_mod[[1]] %>%
+#     mod_intuitive_to_draws_df() %>%
+#     draws_df_to_logit_plot()
+
+library(furrr)
+options(future.fork.enable = TRUE)
+plan(multiprocess)
+
+out <-
+    tibble(n_dif_items = 2:6) %>%
+    mutate(
+        bigsim = n_dif_items %>% future_map(~ bigsim(runs = 20, n_dif_items = .))
+    )
+
+Sys.time()
+
+out %>% write_rds("paper/paper_3_out1.rds")
